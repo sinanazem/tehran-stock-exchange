386acbb5fc0f5131f08f5b154b29cf2125f9c174
Sina Nazem <92341812+sinanazem@users.noreply.github.com>
2024-01-22 13:08:08 +0330

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,16 +1,17 @@
+import argparse
 from src.etl import extract, transform, load 
 from src.analysis import stock_analysis
 
-import argparse
 
+if __name__ == "__main__" :
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s','--startdate',help='Start Date Tehran Exchange Market')
-parser.add_argument('-e','--enddate',help='End Date Tehran Exchange Market')
-parser.add_argument('-d','--delete',help='Delete Excel Files')
-args = parser.parse_args()
-
-
-extract.download_tehran_stock_exchange_files(args.startdate, args.enddate)
-load.change_format_store("src/stage/", bool(args.delete))
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s','--startdate',help='Start Date Tehran Exchange Market')
+  parser.add_argument('-e','--enddate',help='End Date Tehran Exchange Market')
+  parser.add_argument('-d','--delete',help='Delete Excel Files')
+  args = parser.parse_args()
+  
+  
+  extract.download_tehran_stock_exchange_files(args.startdate, args.enddate)
+  load.change_format_store("src/stage/", bool(args.delete))
 
