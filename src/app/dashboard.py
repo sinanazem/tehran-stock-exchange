6225c92680ab98614246959db2cb0c8f5049159a
Sina Nazem <92341812+sinanazem@users.noreply.github.com>
2024-01-26 18:01:53 +0330

    Update dashboard.py

--- src/app/dashboard.py
+++ src/app/dashboard.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title='Generate blogs',
+    page_icon="🤖",
+    layout='centered',
+    initial_sidebar_state='collapsed'
+    )
 
+@st.cache_data
+def load_lottiefile(filepath: str):
+    with open(filepath,"r") as f:
+        return json.load(f)
+      
 col1, col2 = st.columns(2)
 
 with col1:
